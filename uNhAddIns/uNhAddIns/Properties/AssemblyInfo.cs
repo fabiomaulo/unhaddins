bdbd3d2944ada58ee8d4a2d8c0d2915a5a2a0f77
Fabio Maulo <fabiomaulo@gmail.com>
2007-08-29 06:37:03 +0000

    Solution, projects and base classes for tests.

---
+++ uNhAddIns/uNhAddIns/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("uNhAddIns")]
+[assembly: AssemblyDescription("Unofficial addins for NHibernate.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uNhAddIns")]
+[assembly: AssemblyCopyright("Licensed under LGPL.")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
