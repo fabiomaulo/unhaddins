77536660bca5fe35f3da715b4c7d526f4d958c5a
Dario Quintana <uooopaa@gmail.com>
2007-10-17 17:18:51 +0000

    UserType UpperString added. Cloner using Serialization added. Test for both added too.

---
+++ uNhAddIns/uNhAddIns.Test/UserTypes/UpperStringFixture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using NHibernate;
+using NUnit.Framework;
+
+namespace uNhAddIns.Test.UserTypes
+{
+    [TestFixture]
+    public class UpperStringFixture : TestCase
+    {
+        protected override IList Mappings
+        {
+            get { return new string[] { "UserTypes.UpperStringMappings.hbm.xml" }; }
+        }
+
+        protected override void OnTearDown()
+        {
+            using (ISession s = OpenSession())
+            {
+                s.Delete("from Foo");
+                s.Flush();
+            }
+        }
+
+        [Test]
+        public void SaveObject()
+        {
+            Foo f = new Foo(1, "coco", "mendieta");
+
+            using (ISession s = OpenSession())
+            {
+                s.Save(f);
+                s.Flush();
+            }
+
+            using (ISession s = OpenSession())
+            {
+                Foo upperFoo = s.Get<Foo>(1);
+                
+                Assert.AreEqual(1,upperFoo.Id);
+                Assert.AreEqual("COCO", upperFoo.Name);
+                Assert.AreEqual("mendieta", upperFoo.Description);
+            }
+
+        }
+
+    }
+}
---
+++ uNhAddIns/uNhAddIns/Serialization/Cloner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace uNhAddIns.Serialization
+{
+    public class Cloner
+    {
+        /// <summary>
+        /// Make a object clone via Serializing/Deserealizing the object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static object Clone(object obj)
+        {
+            return Clone(obj, new BinaryFormatter());
+        }
+
+        public static object Clone(object obj, IFormatter formatter)
+        {   
+            using (MemoryStream buffer = new MemoryStream())
+            {
+                formatter.Serialize(buffer,obj);
+                buffer.Position = 0;
+                return formatter.Deserialize(buffer);
+            }
+        }
+ 
+    }
+}
