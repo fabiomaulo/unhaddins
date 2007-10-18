db7c9006d280508da2b732b221dbc34b45ef85b5
Dario Quintana <uooopaa@gmail.com>
2007-10-18 19:24:41 +0000

    Prepare classes for the Pagination example.

---
+++ Examples/uNHAddIns.Examples.Pagination/Data/Check.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NHibernate;
+
+namespace Data
+{
+    public class Check
+    {
+        public static void Ensure(bool b, object s)
+        {
+            if(s == null)
+            {}
+        }
+
+        public static void NotNull(object obj, string s)
+        {
+            if(obj == null)
+                throw new ApplicationException(s);
+        }
+    }
+}
