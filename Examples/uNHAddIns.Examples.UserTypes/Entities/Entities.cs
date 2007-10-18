72476e6d6950b4a7f094c1a4e9829e0031a82ca3
Dario Quintana <uooopaa@gmail.com>
2007-10-18 06:18:54 +0000

    UpperString Example added. Template for examples added.

---
+++ Examples/uNHAddIns.Examples.UserTypes/Entities/Entities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NH
+{
+    public class Customer
+    {
+        private int id;
+
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        private string firstName;
+        
+        public virtual string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        
+        private string lastName;
+
+        public virtual string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }    
+    }
+}
