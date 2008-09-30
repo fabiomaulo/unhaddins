4086d2dadbb1ddcbe47ad93166fca3325995e5ef
Fabio Maulo <fabiomaulo@gmail.com>
2008-09-30 18:43:36 +0000

    Contexts implementations

---
+++ uNhAddIns/uNhAddIns.Test/SessionEasier/Contexts/Other.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace uNhAddIns.Test.SessionEasier.Contexts
+{
+	[Serializable]
+	public class Other
+	{
+		public virtual long Id { get; set; }
+
+		public virtual string Name { get; set; }
+	}
+}
