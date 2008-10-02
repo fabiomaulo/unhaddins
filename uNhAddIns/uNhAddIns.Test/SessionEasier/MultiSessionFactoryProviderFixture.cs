3cfd12b6443f927838528c2e5569d69cdecadbd2
Fabio Maulo <fabiomaulo@gmail.com>
2008-10-02 17:21:31 +0000

    Removed warn for obsolete NUnit methods usage.

--- uNhAddIns/uNhAddIns.Test/SessionEasier/MultiSessionFactoryProviderFixture.cs
+++ uNhAddIns/uNhAddIns.Test/SessionEasier/MultiSessionFactoryProviderFixture.cs
@@ -2,7 +2,7 @@ using System;
 using log4net.Config;
 using NHibernate;
 using NUnit.Framework;
-using NUnit.Framework.SyntaxHelpers;
+using NUnit.Framework.Syntax.CSharp;
 using uNhAddIns.SessionEasier;
 
 namespace uNhAddIns.Test.SessionEasier
