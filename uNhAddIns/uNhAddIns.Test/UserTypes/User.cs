9e5512edc2ea59db331e61cfd60a924de5910ee3
Gustavo Ringel <gustavo.ringel@gmail.com>
2008-12-01 09:32:47 +0000

    Added EncryptedString IUserType which will take care of encrypting decrypting a string. It is a simple encryption. Support for a hashed and salted password will be added as a composite user type.

---
+++ uNhAddIns/uNhAddIns.Test/UserTypes/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace uNhAddIns.Test.UserTypes
+{
+	[Serializable]
+	public class User
+	{
+		private string name;
+		private string password;
+		private int id;
+
+		public User()
+		{
+			
+		}
+
+		public User(int id, string name, string password)
+		{
+			this.id = id;
+			this.password = password;
+			this.name = name;
+		}
+
+		public virtual int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+		
+		public virtual string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public virtual string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+	}
+}
