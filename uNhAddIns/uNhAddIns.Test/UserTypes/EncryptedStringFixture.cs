a2da13eb9ff2f551ce266afaa9e99aa4a4597da7
Gustavo Ringel <gustavo.ringel@gmail.com>
2008-12-01 09:32:47 +0000

    Added EncryptedString IUserType which will take care of encrypting decrypting a string. It is a simple encryption. Support for a hashed and salted password will be added as a composite user type.

---
+++ uNhAddIns/uNhAddIns.Test/UserTypes/EncryptedStringFixture.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using NHibernate;
+using NUnit.Framework;
+
+namespace uNhAddIns.Test.UserTypes
+{
+    [TestFixture]
+	public class EncryptedStringFixture : TestCase
+    {
+        protected override IList Mappings
+        {
+            get { return new string[] { "UserTypes.User.hbm.xml" }; }
+        }
+
+        protected override void OnTearDown()
+        {
+            using (ISession s = OpenSession())
+            {
+                s.Delete("from User");
+                s.Flush();
+            }
+        }
+
+        [Test]
+        public void SaveEncryptedPassword()
+        {
+            User user = new User(1, "Astor Piazolla", "tango");
+
+            using (ISession s = OpenSession())
+            {
+                s.Save(user);
+                s.Flush();
+            }
+
+            using (ISession s = OpenSession())
+            {
+                User decryptedUser = s.Get<User>(1);
+                
+                Assert.AreEqual(1,decryptedUser.Id);
+                Assert.AreEqual("Astor Piazolla", decryptedUser.Name);
+                Assert.AreEqual("tango", decryptedUser.Password);
+            }
+        }
+
+        [Test]
+        public void SaveNullPropertyAndGetItBack()
+        {
+            User user = new User(2, "Pat Metheny", null);
+
+            using (ISession s = OpenSession())
+            {
+                s.Save(user);
+                s.Flush();
+            }
+
+            using (ISession s = OpenSession())
+            {
+                User decryptedUser = s.Get<User>(2);
+
+				Assert.AreEqual(2, decryptedUser.Id);
+				Assert.AreEqual("Pat Metheny", decryptedUser.Name);
+				Assert.IsNull(decryptedUser.Password);
+            }
+
+        }
+    }
+}
