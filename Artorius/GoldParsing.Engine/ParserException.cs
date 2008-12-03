a8a6fad2fa63315fc3c158813280fee436cacdb1
Fabio Maulo <fabiomaulo@gmail.com>
2008-12-03 19:07:57 +0000

    - Prj refactoring with some SVN problems - Add configuration

---
+++ Artorius/GoldParsing.Engine/ParserException.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace GoldParsing.Engine
+{
+	[Serializable]
+	public class ParserException : Exception
+	{
+		public ParserException() {}
+		public ParserException(string message) : base(message) {}
+		public ParserException(string message, Exception inner) : base(message, inner) {}
+
+		protected ParserException(SerializationInfo info,
+		                      StreamingContext context) : base(info, context) {}
+	}
+}
