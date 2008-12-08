a6d649bc617ce7334bf60289b882b339366cf956
Fabio Maulo <fabiomaulo@gmail.com>
2008-12-08 16:46:18 +0000

    - removed unusable test (the compiled grammar are not allowing the change of start node at runtime) - added simple grammar for development scope only - added standalone tool to compile grammars

--- Artorius/Artorius.Tests/BaseParserTestCase.cs
+++ Artorius/Artorius.Tests/BaseParserTestCase.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Linq;
 using GoldParsing.Engine;
 using GoldParsing.Engine.Config;
 using NHibernate.Hql.Ast.GoldImpls;
 
 namespace Artorius.Tests
 {
-	public abstract class BaseParserTestCase
+	public class BaseParserTestCase
 	{
 		public const string GrammarPath = @"..\..\..\Grammar\Hql.cgt";
 		private static readonly IGrammar grammar;
-		private static readonly SyntaxNodeFactory syntaxNodeFactory= new SyntaxNodeFactory();
+		private static readonly SyntaxNodeFactory syntaxNodeFactory = new SyntaxNodeFactory();
 
 		static BaseParserTestCase()
 		{
@@ -18,42 +16,9 @@ namespace Artorius.Tests
 			grammar = cgl.Load();
 		}
 
-		protected abstract string SymbolNameFromWhereStart { get; }
-
 		public HqlParser NewParser()
 		{
-			Grammar shallowCopy = GetShallowCopy(grammar);
-			Symbol whereStart = grammar.SymbolTable.FirstOrDefault(symbol => symbol.Name == SymbolNameFromWhereStart);
-			if (whereStart != null)
-			{
-				shallowCopy.StartSymbolIndex = whereStart.TableIndex;
-			}
-			else
-			{
-				throw new ArgumentException("Symbol name, from where start, not found");
-			}
-			return new HqlParser(shallowCopy, syntaxNodeFactory);
-		}
-
-		private static Grammar GetShallowCopy(IGrammar orgGrammar)
-		{
-			var shallowCopy = new Grammar
-			                  	{
-			                  		CharSetTable = orgGrammar.CharSetTable,
-			                  		DFATable = orgGrammar.DFATable,
-			                  		DFAInitialStateIndex = orgGrammar.DFAInitialStateIndex,
-			                  		IsCaseSensitive = orgGrammar.IsCaseSensitive,
-			                  		LALRInitialStateIndex = orgGrammar.LALRInitialStateIndex,
-			                  		LALRTable = orgGrammar.LALRTable,
-			                  		RuleTable = orgGrammar.RuleTable,
-			                  		SymbolTable = orgGrammar.SymbolTable,
-			                  		StartSymbolIndex = orgGrammar.StartSymbolIndex
-			                  	};
-			foreach (var pair in orgGrammar.Parameters)
-			{
-				shallowCopy.Parameters.Add(pair);
-			}
-			return shallowCopy;
+			return new HqlParser(grammar, syntaxNodeFactory);
 		}
 	}
 }
---
+++ Artorius/Tools/goldbuilder.bat
@@ -0,0 +1,2 @@
+@echo off
+start /wait goldbuilder_main %1 %2 %3 %4 %5
