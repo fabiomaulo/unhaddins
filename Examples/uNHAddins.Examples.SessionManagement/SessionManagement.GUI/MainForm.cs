ece38b07040528aea76a27fe12d391e5d2b85d69
Gustavo Ringel <gustavo.ringel@gmail.com>
2009-01-07 18:49:49 +0000

    NHibernate was referenced in the GUI for the SchemaCreation moved the schema creation to the implementation layers so i don't have bad references.

--- Examples/uNHAddins.Examples.SessionManagement/SessionManagement.GUI/MainForm.cs
+++ Examples/uNHAddins.Examples.SessionManagement/SessionManagement.GUI/MainForm.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Windows.Forms;
-using NHibernate.Cfg;
-using NHibernate.Tool.hbm2ddl;
 using SessionManagement.GUI.Views;
 using SessionManagement.Infrastructure.InversionOfControl;
+using SessionManagement.Data.SchemaCreation;
 
 namespace SessionManagement.GUI
 {
 	public partial class MainForm : Form
 	{
+		private readonly ISchemaCreator schemaCreator;
+
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		public MainForm(ISchemaCreator schemaCreator) : this()
+		{
+			this.schemaCreator = schemaCreator;
 		}
 
 		private void closeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -39,14 +45,12 @@ namespace SessionManagement.GUI
 
 		private void createToolStripMenuItem1_Click(object sender, EventArgs e)
 		{
-			var cfg = new Configuration().Configure();
-			new SchemaExport(cfg).Create(false, true);
+			schemaCreator.CreateSchema();
 		}
 
 		private void dropToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			var cfg = new Configuration().Configure();
-			new SchemaExport(cfg).Drop(false, true);
+			schemaCreator.DropSchema();
 		}
 	}
 }
