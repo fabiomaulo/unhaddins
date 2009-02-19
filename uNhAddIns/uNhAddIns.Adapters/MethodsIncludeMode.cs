96debb93a6a06586c82c6f426702f8dcc63be420
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-19 17:16:45 +0000

    The MethodsIncludeMode default should be Explicit (compatibility with actual behavior)

--- uNhAddIns/uNhAddIns.Adapters/MethodsIncludeMode.cs
+++ uNhAddIns/uNhAddIns.Adapters/MethodsIncludeMode.cs
@@ -6,13 +6,13 @@ namespace uNhAddIns.Adapters
 	public enum MethodsIncludeMode
 	{
 		///<summary>
-		/// Each method is involved in a persistence-conversation if not explicitly excluded.
-		///</summary>
-		Implicit,
-
-		///<summary>
 		/// Methods involved must be explicitly declared.
 		///</summary>
-		Explicit
+		Explicit,
+		
+		///<summary>
+		/// Each method is involved in a persistence-conversation if not explicitly excluded.
+		///</summary>
+		Implicit
 	}
 }
