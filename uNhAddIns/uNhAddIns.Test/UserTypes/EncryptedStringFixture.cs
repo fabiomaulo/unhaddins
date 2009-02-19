3b97bfa9a27903a8d95222f6bf0dff6d73065122
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-19 21:01:13 +0000

    Minor (reformatted)

--- uNhAddIns/uNhAddIns.Test/UserTypes/EncryptedStringFixture.cs
+++ uNhAddIns/uNhAddIns.Test/UserTypes/EncryptedStringFixture.cs
@@ -4,64 +4,63 @@ using NUnit.Framework;
 
 namespace uNhAddIns.Test.UserTypes
 {
-    [TestFixture]
+	[TestFixture]
 	public class EncryptedStringFixture : TestCase
-    {
-        protected override IList Mappings
-        {
-            get { return new string[] { "UserTypes.User.hbm.xml" }; }
-        }
+	{
+		protected override IList Mappings
+		{
+			get { return new[] {"UserTypes.User.hbm.xml"}; }
+		}
 
-        protected override void OnTearDown()
-        {
-            using (ISession s = OpenSession())
-            {
-                s.Delete("from User");
-                s.Flush();
-            }
-        }
+		protected override void OnTearDown()
+		{
+			using (ISession s = OpenSession())
+			{
+				s.Delete("from User");
+				s.Flush();
+			}
+		}
 
-        [Test]
-        public void SaveEncryptedPassword()
-        {
-            User user = new User(1, "Astor Piazolla", "tango");
+		[Test]
+		public void SaveEncryptedPassword()
+		{
+			var user = new User(1, "Astor Piazolla", "tango");
 
-            using (ISession s = OpenSession())
-            {
-                s.Save(user);
-                s.Flush();
-            }
+			using (ISession s = OpenSession())
+			{
+				s.Save(user);
+				s.Flush();
+			}
 
-            using (ISession s = OpenSession())
-            {
-                User decryptedUser = s.Get<User>(1);
-                
-                Assert.AreEqual(1,decryptedUser.Id);
-                Assert.AreEqual("Astor Piazolla", decryptedUser.Name);
-                Assert.AreEqual("tango", decryptedUser.Password);
-            }
-        }
+			using (ISession s = OpenSession())
+			{
+				var decryptedUser = s.Get<User>(1);
 
-        [Test]
-        public void SaveNullPropertyAndGetItBack()
-        {
-            User user = new User(2, "Pat Metheny", null);
+				Assert.AreEqual(1, decryptedUser.Id);
+				Assert.AreEqual("Astor Piazolla", decryptedUser.Name);
+				Assert.AreEqual("tango", decryptedUser.Password);
+			}
+		}
 
-            using (ISession s = OpenSession())
-            {
-                s.Save(user);
-                s.Flush();
-            }
+		[Test]
+		public void SaveNullPropertyAndGetItBack()
+		{
+			var user = new User(2, "Pat Metheny", null);
 
-            using (ISession s = OpenSession())
-            {
-                User decryptedUser = s.Get<User>(2);
+			using (ISession s = OpenSession())
+			{
+				s.Save(user);
+				s.Flush();
+			}
+
+			using (ISession s = OpenSession())
+			{
+				var decryptedUser = s.Get<User>(2);
 
 				Assert.AreEqual(2, decryptedUser.Id);
 				Assert.AreEqual("Pat Metheny", decryptedUser.Name);
 				Assert.IsNull(decryptedUser.Password);
-            }
-
-        }
-    }
+			}
+		}
+	}
 }
