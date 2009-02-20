25e9f4a6e486d8c797e15c76dc0b2e28497e7fff
Gustavo Ringel <gustavo.ringel@gmail.com>
2009-02-20 07:02:35 +0000

    Minor comments corrections

--- uNhAddIns/uNhAddIns.Adapters/PersistenceConversationAttribute.cs
+++ uNhAddIns/uNhAddIns.Adapters/PersistenceConversationAttribute.cs
@@ -3,7 +3,7 @@
 namespace uNhAddIns.Adapters
 {
 	/// <summary>
-	/// Attribute to mark a mathod as involved in a persistence conversation
+	/// Attribute to mark a method as involved in a persistence conversation
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 	public class PersistenceConversationAttribute : Attribute
@@ -17,7 +17,7 @@ namespace uNhAddIns.Adapters
 		public EndMode ConversationEndMode { get; set; }
 
 		///<summary>
-		/// <see langword="true"/> if you want explicit exclude a method from a persistence-conversation.
+		/// <see langword="true"/> if you want to explicitly exclude a method from a persistence-conversation.
 		///</summary>
 		/// <remarks>
 		/// Default <see langword="false"/>.
