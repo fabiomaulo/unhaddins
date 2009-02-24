752ded91c01157f13acff47f9c69c307e629092f
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-24 19:39:40 +0000

    - SessionFactoryProvider full test

--- uNhAddIns/uNhAddIns.Test/SessionEasier/SessionFactoryProviderFixture.cs
+++ uNhAddIns/uNhAddIns.Test/SessionEasier/SessionFactoryProviderFixture.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using log4net.Config;
 using NHibernate;
+using NHibernate.Cfg;
 using NUnit.Framework;
 using uNhAddIns.SessionEasier;
+using uNhAddIns.TestUtils.Logging;
 
 namespace uNhAddIns.Test.SessionEasier
 {
@@ -14,19 +19,45 @@ namespace uNhAddIns.Test.SessionEasier
 			XmlConfigurator.Configure();
 		}
 
-		[Test]
-		public void DisposeWithoutInitialize()
+		public class MultiConfStub : IConfigurationProvider
 		{
-			var sfp = new SessionFactoryProvider();
-			using (var ls = new LogSpy(typeof (SessionFactoryProvider)))
+			#region Implementation of IConfigurationProvider
+
+			public IEnumerable<Configuration> Configure()
 			{
-				sfp.Dispose();
-				Assert.That(ls.GetWholeMessages(), Text.DoesNotContain("Initialize a new session factory"));
+				return new[] {(new Configuration()).Configure(), new Configuration()};
 			}
+
+			public event EventHandler<ConfigurationEventArgs> BeforeConfigure;
+			public event EventHandler<ConfigurationEventArgs> AfterConfigure;
+
+			#endregion
 		}
 
 		[Test]
-		public void Disposing()
+		public void CtorProtectecion()
+		{
+			Assert.Throws<ArgumentNullException>(() => new SessionFactoryProvider(null));
+		}
+
+		[Test]
+		public void DisposeDoNotInitialize()
+		{
+			var sfp = new SessionFactoryProvider();
+			Assert.That(Spying.Logger<SessionFactoryProvider>().Execute(sfp.Dispose).WholeMessage,
+			            Text.DoesNotContain("Initialize a new session factory"));
+		}
+
+		[Test]
+		public void InitializeShouldLog()
+		{
+			var sfp = new SessionFactoryProvider();
+			Assert.That(Spying.Logger<SessionFactoryProvider>().Execute(sfp.Initialize).WholeMessage,
+			            Text.Contains("Initialize a new session factory"));
+		}
+
+		[Test]
+		public void ShouldExecuteDisposeOlnyOne()
 		{
 			SessionFactoryProvider sfp;
 			ISessionFactory sf1;
@@ -37,37 +68,46 @@ namespace uNhAddIns.Test.SessionEasier
 				sf1 = sfp.GetFactory(null);
 			}
 			Assert.That(disposed);
-			Assert.That(sf1.IsClosed);
+			Assert.That(sf1.IsClosed, "The session-factory should be closed.");
 			disposed = false;
 			sfp.Dispose();
 			Assert.That(!disposed);
 		}
 
 		[Test]
-		public void GetSessionFactory()
+		public void ShouldHasOnlyOneInstanceOfFactory()
 		{
 			var sfp = new SessionFactoryProvider();
-			Assert.That(sfp.GetFactory(null), Is.Not.Null);
-			using (var ls = new LogSpy(typeof (SessionFactoryProvider)))
-			{
-				sfp.Initialize();
-				Assert.That(ls.GetWholeMessages(), Text.DoesNotContain("Initialize a new session factory"));
-			}
 			ISessionFactory sf1 = sfp.GetFactory(null);
 			ISessionFactory sf2 = sfp.GetFactory(null);
+			Assert.That(sf1, Is.Not.Null);
 			Assert.That(ReferenceEquals(sf1, sf2));
+			Assert.That(sfp.Count(), Is.EqualTo(1));
+			IEnumerator en = ((IEnumerable)sfp).GetEnumerator();
+			int i = 0;
+			while (en.MoveNext())
+			{
+				i++;
+			}
+			Assert.That(i, Is.EqualTo(1));
 		}
 
 		[Test]
-		public void Initialize()
+		public void ShouldInitializeOnlyOneTime()
 		{
 			var sfp = new SessionFactoryProvider();
-			using (var ls = new LogSpy(typeof (SessionFactoryProvider)))
-			{
-				sfp.Initialize();
-				Assert.That(ls.GetWholeMessages(), Text.Contains("Initialize a new session factory"));
-			}
 			Assert.That(sfp.GetFactory(null), Is.Not.Null);
+
+			Assert.That(Spying.Logger<SessionFactoryProvider>().Execute(sfp.Initialize).WholeMessage,
+			            Text.DoesNotContain("Initialize a new session factory"));
+		}
+
+		[Test]
+		public void ShouldLogMultiConfigurationIgnored()
+		{
+			var sfp = new SessionFactoryProvider(new MultiConfStub());
+			Assert.That(Spying.Logger<SessionFactoryProvider>().Execute(sfp.Initialize).WholeMessage,
+			            Text.Contains("More than one configurations are available"));
 		}
 	}
 }
