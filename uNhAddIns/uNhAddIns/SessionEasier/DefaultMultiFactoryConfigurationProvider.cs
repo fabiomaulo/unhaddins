edc10c508e0eb6ea7f7d1de432d736f5332b5ae5
Fabio Maulo <fabiomaulo@gmail.com>
2009-02-25 16:05:45 +0000

    New organization of configuration - common interface IConfigurationProvider - Two implementations for single and multi RDBMS - Improved BeforeConfigure event to allow custom Configure (BREAKING CHANGE)

--- uNhAddIns/uNhAddIns/SessionEasier/DefaultMultiFactoryConfigurationProvider.cs
+++ uNhAddIns/uNhAddIns/SessionEasier/DefaultMultiFactoryConfigurationProvider.cs
@@ -1,20 +1,14 @@
-using System;
 using System.Collections.Generic;
 using System.Configuration;
 using Configuration=NHibernate.Cfg.Configuration;
 
 namespace uNhAddIns.SessionEasier
 {
-	public class DefaultMultiFactoryConfigurationProvider : IConfigurationProvider
+	public class DefaultMultiFactoryConfigurationProvider : AbstractConfigurationProvider
 	{
 		private const string factoriesStart = "nhfactory";
 
-		#region IConfigurationProvider Members
-
-		public event EventHandler<ConfigurationEventArgs> BeforeConfigure;
-		public event EventHandler<ConfigurationEventArgs> AfterConfigure;
-
-		public IEnumerable<Configuration> Configure()
+		public override IEnumerable<Configuration> Configure()
 		{
 			var result = new List<Configuration>(4);
 			foreach (string setting in ConfigurationManager.AppSettings.Keys)
@@ -23,31 +17,19 @@ namespace uNhAddIns.SessionEasier
 				{
 					string nhConfigFilePath = ConfigurationManager.AppSettings[setting];
 					var configuration = new Configuration();
-					DoBeforeConfigure(configuration);
-					configuration.Configure(nhConfigFilePath);
+
+					bool configured;
+					DoBeforeConfigure(configuration, out configured);
+					if (!configured)
+					{
+						configuration.Configure(nhConfigFilePath);
+					}
 					DoAfterConfigure(configuration);
+
 					result.Add(configuration);
 				}
 			}
 			return result.ToArray();
-		}
-
-		#endregion
-
-		private void DoAfterConfigure(Configuration cfg)
-		{
-			if (AfterConfigure != null)
-			{
-				AfterConfigure(this, new ConfigurationEventArgs(cfg));
-			}
-		}
-
-		private void DoBeforeConfigure(Configuration cfg)
-		{
-			if (BeforeConfigure != null)
-			{
-				BeforeConfigure(this, new ConfigurationEventArgs(cfg));
-			}
 		}
 	}
 }
--- uNhAddIns/uNhAddIns/SessionEasier/DefaultSessionFactoryConfigurationProvider.cs
+++ uNhAddIns/uNhAddIns/SessionEasier/DefaultSessionFactoryConfigurationProvider.cs
@@ -1,42 +1,22 @@
-using System;
 using System.Collections.Generic;
 using NHibernate.Cfg;
 using NHibernate.Util;
 
 namespace uNhAddIns.SessionEasier
 {
-	public class DefaultSessionFactoryConfigurationProvider : IConfigurationProvider
+	public class DefaultSessionFactoryConfigurationProvider : AbstractConfigurationProvider
 	{
-		#region Implementation of IConfigurationProvider
-
-		public IEnumerable<Configuration> Configure()
+		public override IEnumerable<Configuration> Configure()
 		{
 			var cfg = new Configuration();
-			DoBeforeConfigure(cfg);
-			cfg.Configure();
+			bool configured;
+			DoBeforeConfigure(cfg, out configured);
+			if (!configured)
+			{
+				cfg.Configure();
+			}
 			DoAfterConfigure(cfg);
 			return new SingletonEnumerable<Configuration>(cfg);
-		}
-
-		public event EventHandler<ConfigurationEventArgs> BeforeConfigure;
-		public event EventHandler<ConfigurationEventArgs> AfterConfigure;
-
-		#endregion
-
-		private void DoAfterConfigure(Configuration cfg)
-		{
-			if (AfterConfigure != null)
-			{
-				AfterConfigure(this, new ConfigurationEventArgs(cfg));
-			}
-		}
-
-		private void DoBeforeConfigure(Configuration cfg)
-		{
-			if (BeforeConfigure != null)
-			{
-				BeforeConfigure(this, new ConfigurationEventArgs(cfg));
-			}
 		}
 	}
 }
