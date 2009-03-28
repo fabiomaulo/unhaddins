ea143b94ecfe377916fcf88a678e9dc6c352be95
Gustavo Ringel <gustavo.ringel@gmail.com>
2009-03-28 14:56:24 +0000

    Added a pair of projects to configure WCF Facility and an strategy for session management capable of doing Session per request. Wrapping for transactions should be improved, but it works for the example.

---
+++ Examples/uNHAddins.Examples.SessionManagement/SessionManagement.Console/SessionCallContextInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using NHibernate;
+using SessionManagement.Infrastructure.InversionOfControl;
+using SessionManagement.WCF.Host;
+
+namespace SessionManagement.WCF.Host
+{
+	public class SessionCallContextInitializer : ICallContextInitializer
+	{
+		#region ICallContextInitializer Members
+
+		public object BeforeInvoke(InstanceContext instanceContext, IClientChannel channel, Message message)
+		{
+			var session = IoC.Resolve<ISessionFactory>().GetCurrentSession();
+			session.BeginTransaction();
+			OperationContext.Current.Extensions.Add(new ApplicationContext(session));
+			return null;
+		}
+
+		public void AfterInvoke(object correlationState)
+		{
+			ApplicationContext.Session.Transaction.Commit();
+			ApplicationContext.CloseSession();
+		}
+
+		#endregion
+	}
+}
