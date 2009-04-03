72cbf937a808f79f6935c2b5b5800eedb45cae41
Fabio Maulo <fabiomaulo@gmail.com>
2009-04-03 06:41:31 +0000

    TolerantQueryCache implemented

---
+++ uNhAddIns/uNhAddIns.Test/Cache/Entities.cs
@@ -0,0 +1,12 @@
+namespace uNhAddIns.Test.Cache
+{
+	public class MusicCD
+	{
+		public virtual string Name { get; set; }
+	}
+
+	public class Antique
+	{
+		public virtual string Name { get; set; }
+	}
+}
--- uNhAddIns/uNhAddIns/Cache/TolerantQueryCache.cs
+++ uNhAddIns/uNhAddIns/Cache/TolerantQueryCache.cs
@@ -1,16 +1,41 @@
 using System.Collections.Generic;
+using Iesi.Collections.Generic;
 using NHibernate.Cache;
 using NHibernate.Cfg;
 
 namespace uNhAddIns.Cache
 {
-	public class TolerantQueryCache: StandardQueryCache
+	public class TolerantQueryCache : StandardQueryCache
 	{
-		public TolerantQueryCache(Settings settings, IDictionary<string, string> props, UpdateTimestampsCache updateTimestampsCache, string regionName) : base(settings, props, updateTimestampsCache, regionName)
+		private readonly HashSet<string> toleratedSpaces;
+
+		public TolerantQueryCache(Settings settings, IDictionary<string, string> props,
+		                          UpdateTimestampsCache updateTimestampsCache, string regionName)
+			: base(settings, props, updateTimestampsCache, regionName)
 		{
-			ToleratedSpaces = props.GetQueryCacheRegionTolerance(regionName);
+			toleratedSpaces = new HashSet<string>(props.GetQueryCacheRegionTolerance(regionName));
 		}
 
-		public IEnumerable<string> ToleratedSpaces { get; private set; }
+		public IEnumerable<string> ToleratedSpaces
+		{
+			get { return toleratedSpaces; }
+		}
+
+		protected override bool IsUpToDate(ISet<string> spaces, long timestamp)
+		{
+			if (spaces.Count > 0 && IsTolerated(spaces))
+			{
+				return true;
+			}
+			else
+			{
+				return base.IsUpToDate(spaces, timestamp);
+			}
+		}
+
+		public virtual bool IsTolerated(IEnumerable<string> spaces)
+		{
+			return toleratedSpaces.IsSupersetOf(spaces);
+		}
 	}
 }
