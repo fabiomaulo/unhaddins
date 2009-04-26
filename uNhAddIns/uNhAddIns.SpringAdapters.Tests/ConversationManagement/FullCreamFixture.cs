eccda440e52e0c8c0529c940e66e3898532d7f86
Fabio Maulo <fabiomaulo@gmail.com>
2009-04-26 04:10:37 +0000

    FullCreamFixture working

--- uNhAddIns/uNhAddIns.SpringAdapters.Tests/ConversationManagement/FullCreamFixture.cs
+++ uNhAddIns/uNhAddIns.SpringAdapters.Tests/ConversationManagement/FullCreamFixture.cs
@@ -2,24 +2,33 @@ using CommonServiceLocator.SpringAdapter;
 using Microsoft.Practices.ServiceLocation;
 using NUnit.Framework;
 using Spring.Context;
+using uNhAddIns.Adapters.CommonTests;
 using uNhAddIns.Adapters.CommonTests.Integration;
 using Spring.Context.Support;
+using uNhAddIns.SessionEasier;
 
 namespace uNhAddIns.SpringAdapters.Tests.ConversationManagement
 {
-	[TestFixture, Ignore("Not implemented yet")]
+	[TestFixture]
 	public class FullCreamFixture : FullCreamFixtureBase
 	{
 		#region Overrides of FullCreamFixtureBase
 
 		protected override void InitializeServiceLocator()
 		{
-			IConfigurableApplicationContext context = new XmlApplicationContext();
+			// TODO: Make it work trough XML
+			IConfigurableApplicationContext context = new StaticApplicationContext();
 			var objectFactory = context.ObjectFactory;
+			objectFactory.RegisterDefaultConversationAop();
+			objectFactory.RegisterDefaultPersistentConversationServices();
 			var sl = new SpringServiceLocatorAdapter(objectFactory);
 			objectFactory.RegisterInstance<IServiceLocator>(sl);
-
 			ServiceLocator.SetLocatorProvider(() => sl);
+
+			objectFactory.RegisterInstance(ServiceLocator.Current.GetInstance<ISessionFactoryProvider>().GetFactory(null));
+			objectFactory.Register<IDaoFactory, DaoFactory>();
+			objectFactory.Register<ISillyDao, SillyDao>();
+			objectFactory.RegisterPrototype<ISillyCrudModel, Adapters.CommonTests.SillyCrudModel>();
 		}
 
 		#endregion
