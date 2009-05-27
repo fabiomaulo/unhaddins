95602787d09559c4448e1d50aba066abdd60d935
Fabio Maulo <fabiomaulo@gmail.com>
2009-05-27 20:24:03 +0000

    Continue implementation of MappingAuditableMetaDataStore

--- uNhAddIns/uNhAddIns.Test/Audit/MappingAuditableMetaDataStoreFixture.cs
+++ uNhAddIns/uNhAddIns.Test/Audit/MappingAuditableMetaDataStoreFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NHibernate.Cfg;
 using NUnit.Framework;
 using uNhAddIns.Audit;
@@ -40,6 +41,19 @@ namespace uNhAddIns.Test.Audit
 			store.RegisterAuditableEntityIfNeeded(string.Empty).Should().Be.False();
 			store.GetAuditableMetaData(string.Empty).Should().Be.Null();
 			store.Contains(string.Empty).Should().Be.False();
+		}
+
+		[Test]
+		[Description("Should register all properties.")]
+		public void RegisterProperties()
+		{
+			var conf = new Configuration().Configure();
+			conf.AddResource("uNhAddIns.Test.Audit.Simple.hbm.xml", GetType().Assembly);
+			IAuditableMetaDataStore store = new MappingAuditableMetaDataStore(conf);
+			store.RegisterAuditableEntityIfNeeded(EntityName);
+			var meta = store.GetAuditableMetaData(EntityName);
+			meta.Propeties.Count().Should().Be.GreaterThan(0);
+			meta.Propeties.First().Should().Be.EqualTo("Description");
 		}
 	}
 }
