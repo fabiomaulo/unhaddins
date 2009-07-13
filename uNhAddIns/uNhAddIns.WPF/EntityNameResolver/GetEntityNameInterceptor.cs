988ba00611e13b42325cbd8febd0f8732465a9c2
Jose Romaniello <jfromaniello@gmail.com>
2009-07-13 22:24:22 +0000

    Entity name resolved embedded in  uNhAddIns.WPF temporally.

---
+++ uNhAddIns/uNhAddIns.WPF/EntityNameResolver/GetEntityNameInterceptor.cs
@@ -0,0 +1,31 @@
+using Castle.Core;
+using Castle.Core.Interceptor;
+
+namespace uNhAddIns.WPF.EntityNameResolver
+{
+    public class GetEntityNameInterceptor : IInterceptor, IOnBehalfAware
+    {
+        private string _entityName;
+
+        #region IInterceptor Members
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (invocation.Method.Name == "get_EntityName")
+            {
+                invocation.ReturnValue = _entityName;
+            }
+        }
+
+        #endregion
+
+        #region IOnBehalfAware Members
+
+        public void SetInterceptedComponentModel(ComponentModel target)
+        {
+            _entityName = target.Implementation.FullName;
+        }
+
+        #endregion
+    }
+}
---
+++ uNhAddIns/uNhAddIns.WPF/EntityNameResolver/readme.txt
@@ -0,0 +1,2 @@
+﻿This folder should not be here.
+http://groups.google.com.ar/group/unhaddins/t/fa420980d4e071b6
