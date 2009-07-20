a5f6b22ee6cdc807537d183fd3ef42fc521aeef3
Jose Romaniello <jfromaniello@gmail.com>
2009-07-20 15:18:02 +0000

    refactoring (minor)

--- uNhAddIns/uNhAddIns.WPF.Castle/PropertyChangeNotifier.cs
+++ uNhAddIns/uNhAddIns.WPF.Castle/PropertyChangeNotifier.cs
@@ -3,54 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Castle.Core.Interceptor;
+using System.Linq;
 
 namespace uNhAddIns.WPF.Castle
 {
     public class PropertyChangeNotifier : PropertyChangeNotifierBase, IInterceptor
     {
-        private bool _isInEditMode = false;
-        private readonly List<string> _editedProperties = new List<string>();
-
         #region IInterceptor Members
 
         public void Intercept(IInvocation invocation)
         {
             string methodName = invocation.Method.Name;
-            if (methodName == "add_PropertyChanged")
-            {
-                StoreHandler((Delegate) invocation.Arguments[0]);
+            object[] arguments = invocation.Arguments;
+            object proxy = invocation.Proxy;
+            bool isEditableObject = proxy is IEditableObject;
+
+            GrabEventsHandlers(methodName, arguments);
+
+            if (!ShouldProceedWithInvocation(methodName))
                 return;
-            }
-            if (methodName == "remove_PropertyChanged")
-            {
-                RemoveHandler((Delegate) invocation.Arguments[0]);
-                return;
-            }
 
             invocation.Proceed();
 
-            if("BeginEdit".Equals(methodName) && invocation.Proxy is IEditableObject)
-            {
-                _isInEditMode = true;
-            }
-            if("CancelEdit".Equals(methodName) && invocation.Proxy is IEditableObject)
-            {
-                _isInEditMode = false;
-                _editedProperties.ForEach(p => OnPropertyChanged(invocation.Proxy, new PropertyChangedEventArgs(p)));
-            }
-            
-            if (invocation.MethodInvocationTarget.Name.StartsWith("set_"))
-            {
-                string propertyName = methodName.Substring(4);
-
-                if(_isInEditMode)
-                {
-                    _editedProperties.Add(propertyName);
-                }
-                
-                var args = new PropertyChangedEventArgs(propertyName);
-                OnPropertyChanged(invocation.Proxy, args);
-            }
+            NotifyPropertyChanged(methodName, proxy, isEditableObject);
         }
 
         #endregion
