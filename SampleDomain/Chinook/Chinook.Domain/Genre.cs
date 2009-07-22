7089c0e9f47b31c86ca04f876bb65609da41ea30
Jose Romaniello <jfromaniello@gmail.com>
2009-07-22 23:13:24 +0000

    Chinook sample domain.

---
+++ SampleDomain/Chinook/Chinook.Domain/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chinook.Domain
+{
+    public class Genre
+    {
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
