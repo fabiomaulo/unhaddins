7bdc9de60b5d342faa700ee5a0e778b2d32d04cb
Jose Romaniello <jfromaniello@gmail.com>
2009-07-24 18:22:45 +0000

    AsObservableWithAutoPersistence

--- uNhAddIns/uNhAddIns.WPF/CollectionsExtension.cs
+++ uNhAddIns/uNhAddIns.WPF/CollectionsExtension.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using NHibernate;
+using NHibernate.Engine;
 
 namespace uNhAddIns.WPF
 {
@@ -8,6 +11,44 @@ namespace uNhAddIns.WPF
         public static ObservableCollection<T> AsObservable<T>(this IEnumerable<T> coll)
         {
             return new ObservableCollection<T>(coll);
+        }
+
+        /// <summary>
+        /// untested.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="coll"></param>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        public static ObservableCollection<T> AsObservableWithAutoPersistence<T>(this IEnumerable<T> coll, ISession session)
+        {
+            var observableCollection = new ObservableCollection<T>(coll);
+
+            observableCollection.CollectionChanged 
+                += (sender, args) =>
+                {
+                    switch (args.Action)
+                    {
+                        case NotifyCollectionChangedAction.Add:
+                            foreach (var newItem in args.NewItems)
+                            {
+                                session.Save(newItem);
+                            }
+                            break;
+                        case NotifyCollectionChangedAction.Remove:
+                            foreach (var newItem in args.NewItems)
+                            {
+                                session.Delete(newItem);
+                            }
+                            break;
+                        case NotifyCollectionChangedAction.Reset:
+                            //ouch
+                            break;
+                        default:
+                            break;
+                    }
+                };
+            return observableCollection;
         }
     }
 }
