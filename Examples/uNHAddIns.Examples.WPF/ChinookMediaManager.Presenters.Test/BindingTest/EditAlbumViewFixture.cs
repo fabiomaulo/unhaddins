2d688fa80f9ebd8158230ca196d87651d1e1a94e
Jose Romaniello <jfromaniello@gmail.com>
2009-07-28 16:44:18 +0000

    Edit album view first test.

---
+++ Examples/uNHAddIns.Examples.WPF/ChinookMediaManager.Presenters.Test/BindingTest/EditAlbumViewFixture.cs
@@ -0,0 +1,20 @@
+using Caliburn.Testability;
+using ChinookMediaManager.GUI.Views;
+using ChinookMediaManager.Presenters.Interfaces;
+using NUnit.Framework;
+
+namespace ChinookMediaManager.Presenters.Test.BindingTest
+{
+    [TestFixture]
+    public class EditAlbumViewFixture
+    {
+        [Test]
+        public void is_data_bound_to_album()
+        {
+            var validator = Validator.For<EditAlbumView, IEditAlbumPresenter>();
+            var result = validator.Validate();
+            result.WasBoundTo(p => p.Album.Title).Should().Be.True();
+            result.WasBoundTo(p => p.Album.Artist.Name).Should().Be.True();
+        }
+    }
+}
---
+++ Examples/uNHAddIns.Examples.WPF/ChinookMediaManager.Presenters.Test/EditAlbumPresenterFixture.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace ChinookMediaManager.Presenters.Test
+{
+    [TestFixture]
+    public class EditAlbumPresenterFixture
+    {
+        
+
+    }
+}
