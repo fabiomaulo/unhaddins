459f6ce39413402864eca0e2f1c9655fea17727e
Jose Romaniello <jfromaniello@gmail.com>
2009-07-30 13:55:16 +0000

    Added EditableCollection.

---
+++ uNhAddIns/uNhAddIns.WPF/Collections/EditableCollection.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace uNhAddIns.WPF.Collections
+{
+    public class EditableCollection<T> : ObservableCollection<T>, IEditableObject
+    {
+        private IList<T> _bakupList;
+        private bool _isInEditMode;
+
+        #region IEditableObject Members
+
+        public void BeginEdit()
+        {
+            _bakupList = new List<T>();
+            Items.ForEach(_bakupList.Add);
+            _isInEditMode = true;
+        }
+
+        public void EndEdit()
+        {
+            if (!_isInEditMode)
+                throw new InvalidOperationException("EndEdit without a BeginEdit"); 
+
+            _isInEditMode = false;
+            _bakupList = null;
+        }
+
+        public void CancelEdit()
+        {
+            if(!_isInEditMode)
+                throw new InvalidOperationException("CancelEdit without a BeginEdit"); 
+            _isInEditMode = false;
+            ClearItems();
+            _bakupList.ForEach(Add);
+        }
+
+        #endregion
+    }
+}
