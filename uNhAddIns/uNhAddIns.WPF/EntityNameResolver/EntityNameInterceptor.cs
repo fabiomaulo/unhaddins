3e8c41567d4dec18a92f2b9e35a5f1f36311a8e2
Jose Romaniello <jfromaniello@gmail.com>
2009-08-10 17:06:23 +0000

    Temporary fix for EntityName resolver to work with Session.Load and Session.Refresh.

---
+++ uNhAddIns/uNhAddIns.WPF/EntityNameResolver/EntityNameInterceptor.cs
@@ -0,0 +1,18 @@
+﻿using NHibernate;
+
+namespace uNhAddIns.WPF.EntityNameResolver
+{
+    public class EntityNameInterceptor : EmptyInterceptor
+    {
+        public override string GetEntityName(object entity)
+        {
+            if(entity is INamedEntity)
+            {
+                var namedEntity = (INamedEntity)entity;
+                return namedEntity.EntityName;
+            }
+
+            return base.GetEntityName(entity);
+        }
+    }
+}
