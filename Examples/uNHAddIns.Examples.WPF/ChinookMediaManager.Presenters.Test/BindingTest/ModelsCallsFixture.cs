46df50e9e68dafdeb5af85d2d085017d036a2a52
Jose Romaniello <jfromaniello@gmail.com>
2009-08-10 20:48:00 +0000

    "All  MODELS call from Presenters  (this means calls that use nhibernate session) should be made inside [AsyncActions] methods."

---
+++ Examples/uNHAddIns.Examples.WPF/ChinookMediaManager.Presenters.Test/BindingTest/ModelsCallsFixture.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Caliburn.PresentationFramework.Actions;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using NUnit.Framework;
+
+namespace ChinookMediaManager.Presenters.Test.BindingTest
+{
+    [TestFixture]
+    public class ModelsCallsFixture
+    {
+        IEnumerable<string> models;
+        AssemblyDefinition presenters;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            models = AssemblyFactory
+                .GetAssembly("ChinookMediaManager.Domain.dll")
+                .MainModule.Types.OfType<TypeReference>()
+                .Where(t => t.Namespace.EndsWith("Model"))
+                .Select(m => m.FullName);
+            presenters = AssemblyFactory.GetAssembly("ChinookMediaManager.Presenters.dll");
+        }
+
+
+        private bool MethodIsAsync(ICustomAttributeProvider methodDefinition)
+        {
+            IEnumerable<string> query = methodDefinition.CustomAttributes
+                .OfType<CustomAttribute>()
+                .Select(c => c.Constructor.DeclaringType.FullName);
+
+            return query.Contains(typeof (AsyncActionAttribute).FullName);
+        }
+
+        
+        private void should_not_contains_a_modell_call(IMemberReference type, 
+                                                 MethodDefinition method,
+                                                 IEnumerable<string> models)
+        {
+            if(method.Body == null) return;
+
+            foreach (Instruction instruction in method.Body.Instructions)
+            {
+                if (instruction.Operand == null) continue;
+
+                var methodOperand = instruction.Operand as MemberReference;
+                if (methodOperand != null)
+                    if (models.Contains(methodOperand.DeclaringType.FullName))
+                        ThrowAssertException(type, method, methodOperand);
+
+                var methodCall = instruction.Operand as MethodReference;
+                if (methodCall != null)
+                {
+                    var methodDef = methodCall.GetOriginalMethod() as MethodDefinition;
+                    if(methodDef != null)
+                    {
+                        //recursive-call
+                        should_not_contains_a_modell_call(
+                            methodCall.DeclaringType,
+                            methodDef, models);
+                    }
+                }
+            }
+        }
+
+        private static void ThrowAssertException(IMemberReference type, IMemberReference method,
+                                                 MemberReference methodOperand)
+        {
+            throw new AssertionException(
+                string.Format("The method {0} in the class {1} uses the model {2} but is not async."
+                              , method.Name, type.Name, methodOperand.DeclaringType.Name));
+        }
+
+        /// <summary>
+        ///"All  MODELS call from Presenters  (this means calls that use nhibernate session) 
+        /// should be made inside [AsyncActions] methods."
+        /// </summary>
+        [Test]
+        public void models_calls_ocurrs_on_async_thread()
+        {
+            var concreteTypes = presenters.MainModule
+                                .Types.OfType<TypeDefinition>()
+                                .Where(t => !t.IsInterface && !t.IsAbstract);
+            foreach (TypeDefinition type in concreteTypes)
+            {
+                //public and not async methods.
+                var publicMethods = type.Methods.OfType<MethodDefinition>()
+                                        .Where(m => m.IsPublic && !MethodIsAsync(m));
+
+                foreach (MethodDefinition method in publicMethods)
+                {
+                    should_not_contains_a_modell_call(type, method, models);
+                }
+            }
+        }
+    }
+}
