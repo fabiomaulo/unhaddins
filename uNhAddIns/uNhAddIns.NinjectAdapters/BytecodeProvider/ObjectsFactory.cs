ea092eeb21b052d95a212a87a8776091cd0eaf94
Jose Romaniello <jfromaniello@gmail.com>
2009-09-01 14:00:57 +0000

    new Ninject Adapter

---
+++ uNhAddIns/uNhAddIns.NinjectAdapters/BytecodeProvider/ObjectsFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NHibernate.Bytecode;
+using Ninject;
+
+namespace uNhAddIns.NinjectAdapters.BytecodeProvider
+{
+    public class ObjectsFactory : IObjectsFactory 
+    {
+        public ObjectsFactory(IKernel Kernel)
+        {
+            kernel = Kernel;
+        }
+
+        private readonly IKernel kernel;
+
+        #region IObjectsFactory Members
+
+        object IObjectsFactory.CreateInstance(System.Type type, params object[] ctorArgs)
+        {
+            return Activator.CreateInstance(type, ctorArgs);
+        }
+
+        object IObjectsFactory.CreateInstance(System.Type type, bool nonPublic)
+        {
+            return Activator.CreateInstance(type, nonPublic);
+        }
+
+        object IObjectsFactory.CreateInstance(System.Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+
+        #endregion
+    }
+}
