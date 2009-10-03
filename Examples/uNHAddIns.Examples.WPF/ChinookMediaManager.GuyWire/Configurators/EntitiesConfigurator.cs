746e9e508d2cf3c793f8fd5fd9090d8587f131f9
Jose Romaniello <jfromaniello@gmail.com>
2009-10-03 22:40:21 +0000

    New configuration of the behaviors.

--- Examples/uNHAddIns.Examples.WPF/ChinookMediaManager.GuyWire/Configurators/EntitiesConfigurator.cs
+++ Examples/uNHAddIns.Examples.WPF/ChinookMediaManager.GuyWire/Configurators/EntitiesConfigurator.cs
@@ -1,29 +1,34 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using ChinookMediaManager.Domain;
 using ChinookMediaManager.Infrastructure;
+using uNhAddIns.ComponentBehaviors;
 using uNhAddIns.ComponentBehaviors.Castle;
-using uNhAddIns.WPF.Collections;
-
+using uNhAddIns.ComponentBehaviors.Castle.Configuration;
 
 namespace ChinookMediaManager.GuyWire.Configurators
 {
-    public class EntitiesConfigurator : IConfigurator
-    {
-        public void Configure(IWindsorContainer container)
-        {
-            container.AddFacility<ComponentBehaviorsFacility>();
+	public class EntitiesConfigurator : IConfigurator
+	{
+		#region IConfigurator Members
 
-            container.Register(Component.For<Album>().DependsOn(Property.ForKey("Tracks").Eq(new ObservableCollection<Track>()))
-                                    .NhibernateEntity()
-                                    .AddDataErrorInfoBehavior()
-                                    .AddNotificableBehavior()
-                                    .LifeStyle.Transient);
+		public void Configure(IWindsorContainer container)
+		{
+			container.AddFacility<ComponentBehaviorsFacility>();
+            var config = new BehaviorDictionary();
+			config.For<Album>().Add<DataErrorInfoBehavior>()
+							.Add<NotifyPropertyChangedBehavior>();
+			container.Register(Component.For<IBehaviorStore>().Instance(config));
 
-            container.Register(Component.For<IEntityFactory>()
-                                        .ImplementedBy<EntityFactory>());
-        }
-    }
+			container.Register(Component.For<Album>()
+								.OnCreate((kernel, album) => album.Tracks = new ObservableCollection<Track>())
+								.LifeStyle.Transient);
+
+			container.Register(Component.For<IEntityFactory>()
+			                   	.ImplementedBy<EntityFactory>());
+		}
+
+		#endregion
+	}
 }
