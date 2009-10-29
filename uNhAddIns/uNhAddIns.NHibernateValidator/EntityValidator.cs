35776208e75326f438e2c970e4027bc80680156f
Fabio Maulo <fabiomaulo@gmail.com>
2009-10-29 03:54:37 +0000

    Refactoring

--- uNhAddIns/uNhAddIns.NHibernateValidator/EntityValidator.cs
+++ uNhAddIns/uNhAddIns.NHibernateValidator/EntityValidator.cs
@@ -7,66 +7,46 @@ using uNhAddIns.Adapters;
 
 namespace uNhAddIns.NHibernateValidator
 {
-    public class EntityValidator : IEntityValidator
-    {
-        private readonly ValidatorEngine _validatorEngine;
+	public class EntityValidator : IEntityValidator
+	{
+		private readonly ValidatorEngine validatorEngine;
 
-        public EntityValidator(ValidatorEngine validatorEngine)
-        {
-            _validatorEngine = validatorEngine;
-        }
+		public EntityValidator(ValidatorEngine validatorEngine)
+		{
+			this.validatorEngine = validatorEngine;
+		}
 
-        ///<summary>
-        /// Returns true if the entity is valid.
-        ///</summary>
-        ///<param name="entityInstance"></param>
-        ///<returns></returns>
-        public bool IsValid(object entityInstance)
-        {
-            return _validatorEngine.IsValid(entityInstance);
-        }
+		#region IEntityValidator Members
 
-        ///<summary>
-        /// Validates an entity and returns the information about invalid values.
-        /// </summary>
-        ///<param name="entityInstance"></param>
-        ///<returns></returns>
-        public IList<IInvalidValueInfo> Validate(object entityInstance)
-        {
-            return _validatorEngine.Validate(entityInstance)
-                                   .Select(iv => new InvalidValueInfo(iv) )
-                                   .OfType<IInvalidValueInfo>().ToList();
-        }
+		public bool IsValid(object entityInstance)
+		{
+			return validatorEngine.IsValid(entityInstance);
+		}
 
-        ///<summary>
-        /// Validates a property of the entity and returns the information about invalid values.
-        ///</summary>
-        ///<param name="entityInstance"></param>
-        ///<param name="property"></param>
-        ///<typeparam name="T"></typeparam>
-        ///<typeparam name="TP"></typeparam>
-        ///<returns></returns>
-        public IList<IInvalidValueInfo> Validate<T, TP>(T entityInstance, Expression<Func<T, TP>> property) where T : class
-        {
-            return _validatorEngine.ValidatePropertyValue(entityInstance, property)
-                                   .Select(iv => new InvalidValueInfo(iv))
-                                   .OfType<IInvalidValueInfo>().ToList();
-        }
+		public IList<IInvalidValueInfo> Validate(object entityInstance)
+		{
+			return
+				validatorEngine.Validate(entityInstance)
+				.Select(iv => new InvalidValueInfo(iv))
+				.Cast<IInvalidValueInfo>().ToList();
+		}
 
-        ///<summary>
-        /// Validates a property of the entity and returns the information about invalid values.
-        ///</summary>
-        ///<param name="entityInstance"></param>
-        ///<param name="property"></param>
-        ///<typeparam name="T"></typeparam>
-        ///<typeparam name="TP"></typeparam>
-        ///<returns></returns>
-        public IList<IInvalidValueInfo> Validate(object entityInstance, string property)
-        {
-            return _validatorEngine.ValidatePropertyValue(entityInstance, property)
-                                   .Select(iv => new InvalidValueInfo(iv))
-                                   .OfType<IInvalidValueInfo>()
-                                   .ToList();
-        }
-    }
+		public IList<IInvalidValueInfo> Validate<T, TP>(T entityInstance, Expression<Func<T, TP>> property) where T : class
+		{
+			return
+				validatorEngine.ValidatePropertyValue(entityInstance, property)
+				.Select(iv => new InvalidValueInfo(iv))
+				.Cast<IInvalidValueInfo>().ToList();
+		}
+
+		public IList<IInvalidValueInfo> Validate(object entityInstance, string property)
+		{
+			return
+				validatorEngine.ValidatePropertyValue(entityInstance, property)
+				.Select(iv => new InvalidValueInfo(iv))
+				.Cast<IInvalidValueInfo>().ToList();
+		}
+
+		#endregion
+	}
 }
--- uNhAddIns/uNhAddIns.NHibernateValidator/InvalidValueInfo.cs
+++ uNhAddIns/uNhAddIns.NHibernateValidator/InvalidValueInfo.cs
@@ -1,45 +1,35 @@
 using System;
-using System.Reflection;
 using NHibernate.Validator.Engine;
 using uNhAddIns.Adapters;
 
 namespace uNhAddIns.NHibernateValidator
 {
-    public class InvalidValueInfo : IInvalidValueInfo
-    {
-        private readonly InvalidValue _invalidValue;
+	public class InvalidValueInfo : IInvalidValueInfo
+	{
+		private readonly InvalidValue invalidValue;
 
-        public InvalidValueInfo(InvalidValue invalidValue)
-        {
-            _invalidValue = invalidValue;
-        }
+		public InvalidValueInfo(InvalidValue invalidValue)
+		{
+			this.invalidValue = invalidValue;
+		}
 
-        /// <summary>
-        /// This is the class type that the validation result is applicable to. For instance,
-        /// if the validation result concerns a duplicate record found for an employee, then
-        /// this property would hold the typeof(Employee). It should be expected that this
-        /// property will never be null.
-        /// </summary>
-        public Type EntityType
-        {
-            get { return _invalidValue.EntityType; }
-        }
+		#region IInvalidValueInfo Members
 
-        /// <summary>
-        /// If the validation result is applicable to a specific property, then this
-        /// <see cref="PropertyInfo" /> would be set to a property name.
-        /// </summary>
-        public string PropertyName
-        {
-            get { return _invalidValue.PropertyName; }
-        }
+		public Type EntityType
+		{
+			get { return invalidValue.EntityType; }
+		}
 
-        /// <summary>
-        /// Holds the message describing the validation result for the EntityType and/or PropertyContext
-        /// </summary>
-        public string Message
-        {
-            get { return _invalidValue.Message; }
-        }
-    }
+		public string PropertyName
+		{
+			get { return invalidValue.PropertyName; }
+		}
+
+		public string Message
+		{
+			get { return invalidValue.Message; }
+		}
+
+		#endregion
+	}
 }
