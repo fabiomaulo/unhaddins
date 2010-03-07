5fe33a0d6a0d0764aa05eb8e2c024bdd11a8e71f
Jose Romaniello <jfromaniello@gmail.com>
2010-03-07 23:41:52 +0000

    Added noopconversationalmarker

---
+++ uNhAddIns/uNhAddIns.PostSharpAdapters/NoopConversationalMarker.cs
@@ -0,0 +1,8 @@
+namespace uNhAddIns.PostSharpAdapters
+{
+	/// <summary>
+	/// If the ServiceLocator can resolve this class, all the code to manage the conversation become ignored.
+	/// </summary>
+	public class NoopConversationalMarker
+	{ }
+}
