95fa24720c4e403bcfbda684b374b6785dfe4a86
Jose Romaniello <jfromaniello@gmail.com>
2010-06-03 01:13:13 +0000

    added base classes for the dataannotations engine

---
+++ uNhAddIns/uNhAddIns.DataAnnotations/Engine/DataAnnotationsEntityInspector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uNhAddIns.DataAnnotations.Engine
+{
+	public class DataAnnotationsEntityInspector
+	{
+		private IDictionary<Type, EntityMetadata> cache 
+			= new Dictionary<Type, EntityMetadata>();
+
+		public EntityMetadata GetMetadata(Type type)
+		{
+			EntityMetadata result;
+			if(!cache.TryGetValue(type, out result))
+			{
+				lock (cache)
+				{
+					cache[type] = result = new EntityMetadata(type);
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ uNhAddIns/uNhAddIns.DataAnnotations/Engine/EntityMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace uNhAddIns.DataAnnotations.Engine
+{
+	public class EntityMetadata
+	{
+		public EntityMetadata(Type type)
+		{
+			ValidationsPerProperty = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+				.ToDictionary(
+					p => p, 
+					p => (IEnumerable<ValidationAttribute>)p.GetCustomAttributes(true).OfType<ValidationAttribute>().ToList());
+		}
+
+		public IDictionary<PropertyInfo, IEnumerable<ValidationAttribute>> ValidationsPerProperty { get; private set; }
+
+		public IDictionary<string, IEnumerable<ValidationAttribute>> ValidationsPerPropertyName
+		{
+			get
+			{
+				return ValidationsPerProperty.ToDictionary(kv => kv.Key.Name, kv => kv.Value);
+			}
+		}
+	}
+}
