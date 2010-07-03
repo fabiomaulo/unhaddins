682eb208715a8334c7dfb0ffecb8e1713001662e
jasondentler <jasondentler@localhost>
2010-07-03 06:02:46 +0000

    Added uNhAddIns.CommonServiceLocatorAdapters & tests

---
+++ uNhAddIns/uNhAddIns.CommonServiceLocatorAdapters.Tests/EntityInjectionFixture.cs
@@ -0,0 +1,36 @@
+﻿using CommonServiceLocator.NinjectAdapter;
+using Microsoft.Practices.ServiceLocation;
+using NHibernate.Bytecode;
+using NHibernate.ByteCode.Castle;
+using NHibernate.Proxy;
+using Ninject;
+using NUnit.Framework;
+using uNhAddIns.Adapters.CommonTests.EnhancedBytecodeProvider;
+using uNhAddIns.CommonServiceLocatorAdapters.BytecodeProvider;
+
+namespace uNhAddIns.CommonServiceLocatorAdapters.Tests
+{
+    [TestFixture]
+    public class EntityInjectionFixture : AbstractEntityInjectionFixture
+    {
+
+
+        protected override void InitializeServiceLocator()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<IProxyFactory>()
+              .To<ProxyFactory>();
+            kernel.Bind<IInvoiceTotalCalculator>()
+              .To<SumAndTaxTotalCalculator>();
+            kernel.Bind<IInvoice>()
+              .To<Invoice>().InTransientScope();
+            IServiceLocator sl = new NinjectServiceLocator(kernel);
+            ServiceLocator.SetLocatorProvider(() => sl);
+        }
+
+        protected override IBytecodeProvider GetBytecodeProvider()
+        {
+            return new CSLBytecodeProvider();
+        }
+    }
+}
