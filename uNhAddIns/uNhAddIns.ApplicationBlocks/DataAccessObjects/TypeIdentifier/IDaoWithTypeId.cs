47acbb240e9de27d55555aaf7579cbcb2fcf46a4
Jose Romaniello <jfromaniello@gmail.com>
2010-08-01 15:28:54 +0000

    deleted application blocks

--- uNhAddIns/uNhAddIns.ApplicationBlocks/DataAccessObjects/TypeIdentifier/IDaoWithTypeId.cs
+++
@@ -1,5 +0,0 @@
-namespace uNhAddIns.ApplicationBlocks.DataAccessObjects.TypeIdentifier {
-    public interface IDaoWithTypeId<TEntity, TIdentifier> {
-        TEntity Get(TIdentifier id);
-    }
-}
--- uNhAddIns/uNhAddIns.ApplicationBlocks/Entities/Entity.cs
+++
@@ -1,7 +0,0 @@
-namespace uNhAddIns.ApplicationBlocks.Entities
-{
-	public class Entity : AbstractEntity<int>
-	{
-		public override int Id { get; set; }
-	}
-}
