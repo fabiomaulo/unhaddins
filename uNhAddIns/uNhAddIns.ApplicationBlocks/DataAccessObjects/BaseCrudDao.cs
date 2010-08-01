8ea92feee65d9f8e5bcdc3c13a8577c65e1b27c1
Jose Romaniello <jfromaniello@gmail.com>
2010-08-01 15:27:26 +0000

    removed aplicationblocks and applicationblocks.dataaccessobjects

--- uNhAddIns/uNhAddIns.ApplicationBlocks/DataAccessObjects/BaseCrudDao.cs
+++
@@ -1,9 +0,0 @@
-using uNhAddIns.ApplicationBlocks.DataAccessObjects.TypeIdentifier;
-using uNhAddIns.SessionEasier;
-
-namespace uNhAddIns.ApplicationBlocks.DataAccessObjects {
-    public class BaseCrudDao<TEntity> : BaseCrudDaoWithTypeId<TEntity, int> {
-        protected BaseCrudDao(ISessionFactoryProvider sessionFactoryProvider) : base(sessionFactoryProvider) {
-        }
-    }
-}
--- uNhAddIns/uNhAddIns.ApplicationBlocks/DataAccessObjects/TypeIdentifier/ICrudDaoWithTypeId.cs
+++
@@ -1,6 +0,0 @@
-namespace uNhAddIns.ApplicationBlocks.DataAccessObjects.TypeIdentifier {
-    public interface ICrudDaoWithTypeId<TEntity, TIdentifier> : IDaoWithTypeId<TEntity, TIdentifier> {
-        TEntity MakePersistent(TEntity entity);
-        void MakeTransient(TEntity entity);
-    }
-}
