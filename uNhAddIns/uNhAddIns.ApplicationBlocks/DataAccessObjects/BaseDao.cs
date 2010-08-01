d49527ac10b7d20c5ef46b11dc7cdfadc3403322
Jose Romaniello <jfromaniello@gmail.com>
2010-08-01 15:27:26 +0000

    removed aplicationblocks and applicationblocks.dataaccessobjects

--- uNhAddIns/uNhAddIns.ApplicationBlocks/DataAccessObjects/BaseDao.cs
+++
@@ -1,9 +0,0 @@
-using uNhAddIns.ApplicationBlocks.DataAccessObjects.TypeIdentifier;
-using uNhAddIns.SessionEasier;
-
-namespace uNhAddIns.ApplicationBlocks.DataAccessObjects {
-    public class BaseDao<TEntity> : BaseDaoWithTypeId<TEntity, int> {
-        protected BaseDao(ISessionFactoryProvider sessionFactoryProvider) : base(sessionFactoryProvider) {
-        }
-    }
-}
--- uNhAddIns/uNhAddIns.ApplicationBlocks/DataAccessObjects/IDao.cs
+++
@@ -1,5 +0,0 @@
-using uNhAddIns.ApplicationBlocks.DataAccessObjects.TypeIdentifier;
-
-namespace uNhAddIns.ApplicationBlocks.DataAccessObjects {
-    public interface IDao<TEntity> : IDaoWithTypeId<TEntity, int> { }
-}
